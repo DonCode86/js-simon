d648a0115d3e592019e4349790d4530126e31917
DonCode <99022919+DonCode86@users.noreply.github.com>
2022-05-12 20:54:27 +0200

    fix

--- js/main.js
+++ js/main.js
@@ -2,39 +2,35 @@
 function getRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
 const randomNum = [];
 const userNumbers = [];
-let i;
-let j;
-while (randomNum.length < 5) {
+let guessedNumbers = [];
+for (let i = 0; i < 5; i++) {
     const generatedNum = getRandomNumber(1, 100);
     if (!randomNum.includes(generatedNum)) {
         randomNum.push(generatedNum)
     }
+    console.log(randomNum[i])
 }
 alert("memorizza i numeri che stanno per uscire!!!!")
 alert(randomNum);
 console.log(randomNum)
-    // alert("Memorizza questi numeri!!: ",
-    //     randomNum);
+    // Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+setTimeout(userInput, 3000);
 
-// Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
-setTimeout(function() {
-    while (userNumbers.length < 5) {
+function userInput() {
+    for (let i = 0; i < randomNum.length; i++) {
         const userNum = Number(prompt("Inserisci i numeri che ricordi, uno alla volta e premi ok"))
         userNumbers.push(userNum)
+        if (randomNum.includes(userNum)) {
+            guessedNumbers.push(userNum)
+        }
     }
-    // for (let i = 0; i < randomNum.length; i++) {
-    //     const generatedNum = getRandomNumber(1, 100);
-    //     if (!randomNum.includes(generatedNum)) {
-    //         randomNum.push(generatedNum)
-    //     }
-    // }
-    // for (let j = 0; j < userNumbers.length; j++) {
-    //     const userNum = Number(prompt("Inserisci i numeri che ricordi, uno alla volta e premi ok"))
-    //     userNumbers.push(userNum)
-    // }
-}, 3000);
-console.log(userNumbers)
-    // console.log(userNumbers)
-    // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+    console.log("Hai indovinato un totale di", guessedNumbers.length, "numeri.")
+    console.log("I numeri che hai indovinato sono:", guessedNumbers)
+
+}
+
+
+// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
